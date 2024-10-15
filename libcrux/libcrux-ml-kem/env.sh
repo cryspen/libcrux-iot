ca9c5e9c9411ac6093982e6c5478bbdaa8e5ba56
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-15 11:10:15 +0200

    Remove C extraction from ML-KEM

--- libcrux/libcrux-ml-kem/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CHARON_HOME=/home/jonas/git/charon
-export KRML_HOME=/home/jonas/git/karamel
-export EURYDICE_HOME=/home/jonas/git/eurydice
--- libcrux/libcrux-ml-kem/header_kill_revs.sed
+++
@@ -1,5 +0,0 @@
-/ \* Charon:/d
-/ \* Eurydice:/d
-/ \* Karamel:/d
-/ \* F\*:/d
-/ \* Libcrux:/d
