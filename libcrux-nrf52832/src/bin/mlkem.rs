630efaeba0cf5bfcd92c456399a76e2fd64e7ad2
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-16 15:16:21 +0200

    Initial results for more boards

---
+++ libcrux-nrf52832/src/bin/mlkem.rs
@@ -0,0 +1,30 @@
+#![no_main]
+#![no_std]
+
+use cortex_m::peripheral::Peripherals;
+use libcrux_nrf52832 as _; // global logger + panicking-behavior + memory layout
+
+use core::ptr::addr_of_mut;
+use embedded_alloc::LlffHeap as Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(addr_of_mut!(HEAP_MEM) as usize, HEAP_SIZE) }
+
+        let mut peripherals = Peripherals::take().unwrap();
+        peripherals.DCB.enable_trace();
+        peripherals.DWT.enable_cycle_counter();
+    }
+
+    libcrux_testbench::mlkem::run_benchmarks();
+
+    libcrux_nrf52832::exit()
+}
