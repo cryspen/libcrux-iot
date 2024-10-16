e0514ed88273f4263498161fbeeca9f055c91cd1
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-16 13:42:48 +0200

    Introduce features to test/benchmark different parameter sets

--- libcrux-testbench/src/mldsa.rs
+++ libcrux-testbench/src/mldsa.rs
@@ -4,23 +4,42 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec;
 
-use libcrux_ml_dsa::ml_dsa_65;
+#[cfg(feature = "mldsa44")]
+use libcrux_ml_dsa::ml_dsa_44 as mldsa;
+#[cfg(feature = "mldsa44")]
+type MLDSASignature = mldsa::MLDSA44Signature;
+#[cfg(feature = "mldsa44")]
+type MLDSAKeyPair = mldsa::MLDSA44KeyPair;
+
+#[cfg(feature = "mldsa65")]
+use libcrux_ml_dsa::ml_dsa_65 as mldsa;
+#[cfg(feature = "mldsa65")]
+type MLDSASignature = mldsa::MLDSA65Signature;
+#[cfg(feature = "mldsa65")]
+type MLDSAKeyPair = mldsa::MLDSA65KeyPair;
+
+#[cfg(feature = "mldsa87")]
+use libcrux_ml_dsa::ml_dsa_87 as mldsa;
+#[cfg(feature = "mldsa87")]
+type MLDSASignature = mldsa::MLDSA87Signature;
+#[cfg(feature = "mldsa87")]
+type MLDSAKeyPair = mldsa::MLDSA87KeyPair;
 
 struct MLDSABenchState {
     randomness_gen: [u8; 32],
-    keypair: ml_dsa_65::MLDSA65KeyPair,
+    keypair: MLDSAKeyPair,
     signing_randomness: [u8; 32],
     message: [u8; 1024],
-    signature: ml_dsa_65::MLDSA65Signature,
+    signature: MLDSASignature,
 }
 
 fn bench_keygen<L: EventLogger>(_l: &mut L, state: &MLDSABenchState) -> Result<(), String> {
-    let _pair = ml_dsa_65::generate_key_pair(state.randomness_gen);
+    let _pair = mldsa::generate_key_pair(state.randomness_gen);
     Ok(())
 }
 
 fn bench_sign<L: EventLogger>(_l: &mut L, state: &MLDSABenchState) -> Result<(), String> {
-    let _signature = ml_dsa_65::sign(
+    let _signature = mldsa::sign(
         &state.keypair.signing_key,
         &state.message,
         b"",
@@ -30,7 +49,7 @@ fn bench_sign<L: EventLogger>(_l: &mut L, state: &MLDSABenchState) -> Result<(),
 }
 
 fn bench_verify<L: EventLogger>(_l: &mut L, state: &MLDSABenchState) -> Result<(), String> {
-    let _ = ml_dsa_65::verify(
+    let _ = mldsa::verify(
         &state.keypair.verification_key,
         &state.message,
         b"",
@@ -60,11 +79,10 @@ pub fn run_benchmarks() {
 
     // prepare the state for the benchmarked functions
     let randomness_gen = [1u8; 32];
-    let keypair = ml_dsa_65::generate_key_pair(randomness_gen);
+    let keypair = mldsa::generate_key_pair(randomness_gen);
     let signing_randomness = [4u8; 32];
     let message = [5u8; 1024];
-    let signature =
-        ml_dsa_65::sign(&keypair.signing_key, &message, b"", signing_randomness).unwrap();
+    let signature = mldsa::sign(&keypair.signing_key, &message, b"", signing_randomness).unwrap();
 
     let state = MLDSABenchState {
         randomness_gen,
@@ -78,4 +96,3 @@ pub fn run_benchmarks() {
     let mut logger = DefmtInfoLogger;
     let _ = test_suite.benchmark(&mut logger, &test_config, &state);
 }
-
--- libcrux-testbench/src/mlkem.rs
+++ libcrux-testbench/src/mlkem.rs
@@ -4,28 +4,53 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec;
 
-use libcrux_ml_kem::mlkem768;
+#[cfg(feature = "mlkem512")]
+use libcrux_ml_kem::mlkem512 as mlkem;
+#[cfg(feature = "mlkem512")]
+type MlKemPublicKey = mlkem::MlKem512PublicKey;
+#[cfg(feature = "mlkem512")]
+type MlKemPrivateKey = mlkem::MlKem512PrivateKey;
+#[cfg(feature = "mlkem512")]
+type MlKemCiphertext = mlkem::MlKem512Ciphertext;
+
+#[cfg(feature = "mlkem768")]
+use libcrux_ml_kem::mlkem768 as mlkem;
+#[cfg(feature = "mlkem768")]
+type MlKemPublicKey = mlkem::MlKem768PublicKey;
+#[cfg(feature = "mlkem768")]
+type MlKemPrivateKey = mlkem::MlKem768PrivateKey;
+#[cfg(feature = "mlkem768")]
+type MlKemCiphertext = mlkem::MlKem768Ciphertext;
+
+#[cfg(feature = "mlkem1024")]
+use libcrux_ml_kem::mlkem1024 as mlkem;
+#[cfg(feature = "mlkem1024")]
+type MlKemPublicKey = mlkem::MlKem1024PublicKey;
+#[cfg(feature = "mlkem1024")]
+type MlKemPrivateKey = mlkem::MlKem1024PrivateKey;
+#[cfg(feature = "mlkem1024")]
+type MlKemCiphertext = mlkem::MlKem1024Ciphertext;
 
 struct MlKemBenchState<'a> {
     randomness_gen: [u8; libcrux_ml_kem::KEY_GENERATION_SEED_SIZE],
-    public_key: &'a mlkem768::MlKem768PublicKey,
-    private_key: &'a mlkem768::MlKem768PrivateKey,
+    public_key: &'a MlKemPublicKey,
+    private_key: &'a MlKemPrivateKey,
     randomness_encaps: [u8; libcrux_ml_kem::ENCAPS_SEED_SIZE],
-    ciphertext: mlkem768::MlKem768Ciphertext,
+    ciphertext: MlKemCiphertext,
 }
 
 fn bench_keygen<L: EventLogger>(_l: &mut L, state: &MlKemBenchState) -> Result<(), String> {
-    let _pair = mlkem768::generate_key_pair(state.randomness_gen);
+    let _pair = mlkem::generate_key_pair(state.randomness_gen);
     Ok(())
 }
 
 fn bench_encaps<L: EventLogger>(_l: &mut L, state: &MlKemBenchState) -> Result<(), String> {
-    let _ = mlkem768::encapsulate(&state.public_key, state.randomness_encaps);
+    let _ = mlkem::encapsulate(&state.public_key, state.randomness_encaps);
     Ok(())
 }
 
 fn bench_decaps<L: EventLogger>(_l: &mut L, state: &MlKemBenchState) -> Result<(), String> {
-    let _ = mlkem768::decapsulate(&state.private_key, &state.ciphertext);
+    let _ = mlkem::decapsulate(&state.private_key, &state.ciphertext);
 
     Ok(())
 }
@@ -50,9 +75,9 @@ pub fn run_benchmarks() {
 
     // prepare the state for the benchmarked functions
     let randomness_gen = [1u8; libcrux_ml_kem::KEY_GENERATION_SEED_SIZE];
-    let pair = mlkem768::generate_key_pair(randomness_gen);
+    let pair = mlkem::generate_key_pair(randomness_gen);
     let randomness_encaps = [2u8; libcrux_ml_kem::ENCAPS_SEED_SIZE];
-    let (ciphertext, _shared_secret) = mlkem768::encapsulate(pair.public_key(), randomness_encaps);
+    let (ciphertext, _shared_secret) = mlkem::encapsulate(pair.public_key(), randomness_encaps);
     let state = MlKemBenchState {
         randomness_gen,
         public_key: pair.public_key(),
