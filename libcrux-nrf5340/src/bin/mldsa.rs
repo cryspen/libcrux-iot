1b52f52bd95b48a80e36685205a9bfec7a924f33
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-31 16:07:20 +0100

    Update board crates

--- libcrux-nrf5340/src/bin/mldsa.rs
+++ libcrux-nrf5340/src/bin/mldsa.rs
@@ -22,14 +22,11 @@ fn main() -> ! {
         const HEAP_SIZE: usize = 1024;
         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
         unsafe { HEAP.init(addr_of_mut!(HEAP_MEM) as usize, HEAP_SIZE) }
-
-        let mut peripherals = Peripherals::take().unwrap();
-        peripherals.DCB.enable_trace();
-        peripherals.DWT.enable_cycle_counter();
     }
     
     // set up the test config
     let test_config = TestConfig {
+        platform: platform::CortexM,
         core_freq: board::COREFREQ,
         only_names: alloc::vec![],
         early_abort: false,
