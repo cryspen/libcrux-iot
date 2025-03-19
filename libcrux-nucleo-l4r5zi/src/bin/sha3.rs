1e68f7dbd38433f6bee493d3a80115c8f46231f5
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-03-19 14:18:42 +0100

    SHA3 cycles

---
+++ libcrux-nucleo-l4r5zi/src/bin/sha3.rs
@@ -0,0 +1,75 @@
+#![no_main]
+#![no_std]
+
+use libcrux_nucleo_l4r5zi as board; // global logger + panicking-behavior + memory layout
+
+extern crate alloc;
+
+use embedded_alloc::LlffHeap as Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    // Init rtt-target defmt support
+    // rtt_target::rtt_init_defmt!();
+
+    // Initialize cycle counter
+    {
+        use cortex_m::peripheral::Peripherals;
+        let mut peripherals = Peripherals::take().unwrap();
+        peripherals.DCB.enable_trace();
+        peripherals.DWT.enable_cycle_counter();
+    }
+    let in1 = [1u8; 128];
+    let in2 = [2u8; 1024];
+    let in3 = [3u8; 1024 * 20];
+
+    for _i in 0..3 {
+        let start = CycleCounter::start_measurement();
+        let _d1 = core::hint::black_box(libcrux_sha3::sha256(&in1));
+        core::hint::black_box(CycleCounter::end_measurement("128 bytes", start, &_d1));
+        let start = CycleCounter::start_measurement();
+        let _d2 = core::hint::black_box(libcrux_sha3::sha256(&in2));
+        core::hint::black_box(CycleCounter::end_measurement("1 KB", start, &_d2));
+        let start = CycleCounter::start_measurement();
+        let _d3 = core::hint::black_box(libcrux_sha3::sha256(&in3));
+        core::hint::black_box(CycleCounter::end_measurement("20 KB", start, &_d3));
+    }
+    board::exit()
+}
+
+/// A utility to quickly get cycle counts during execution.
+///
+/// ⚠️ Note, that the hardware must be initialized before the counter
+/// can function.
+pub(crate) struct CycleCounter {}
+
+impl CycleCounter {
+    /// Use this to initialize the hardwar, if it hasn't been initialized elsewhere.
+    #[allow(dead_code)]
+    pub(crate) fn init() {
+        use cortex_m::peripheral::Peripherals;
+        let mut peripherals = Peripherals::take().unwrap();
+        peripherals.DCB.enable_trace();
+        peripherals.DWT.enable_cycle_counter();
+    }
+
+    /// Start measuring cycles.
+    ///
+    /// ⚠️ Using this presupposes that the cycle counter has been initialized somewhere.
+    #[inline(never)]
+    pub(crate) fn start_measurement() -> u32 {
+        cortex_m::peripheral::DWT::cycle_count()
+    }
+
+    /// Report cycles current cycles (Use this to mark the end of measurement).
+    ///
+    /// ⚠️ Using this presupposes that the cycle counter has been initialized somewhere.
+    #[inline(never)]
+    pub(crate) fn end_measurement(msg: &str, start: u32, _input: &[u8]) {
+        let diff = cortex_m::peripheral::DWT::cycle_count() - start;
+        defmt::println!("[END_MEASUREMENT {=str}] : + {=u32}", msg, diff);
+    }
+}
