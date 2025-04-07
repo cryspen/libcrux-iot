29fccf07b8e72d90282e61e4948bdd6b5ef9b122
Jan Winkelmann (keks) <jan@cryspen.com>
2025-04-07 17:20:21 +0200

    add functions and benchmarks for four-rounds versions
    add disassembled versions of libcrux and pqm4
    
    update pqm4 submodule
    
    submodules happy now plz?
    
    update submodule branch
    
    simpler pqm4 submodule commit history

--- libcrux-nucleo-l4r5zi/src/bin/pqm4_cycles.rs
+++ libcrux-nucleo-l4r5zi/src/bin/pqm4_cycles.rs
@@ -42,18 +42,27 @@ fn main() -> ! {
     let mut sha3_output = [0u8; 64];
     let sha3_input = [1, 2, 3, 4];
     unsafe {
-        libcrux_pqm4::sha3_512(addr_of_mut!(sha3_output[0]), addr_of!(sha3_input[0]), 4);
+        libcrux_pqm4::sha3_512(&raw mut sha3_output[0], &raw const sha3_input[0], 4);
     }
     defmt::println!("SHA3-512([1,2,3,4]): {=[u8]}", sha3_output);
 
     let mut sha3_state = [1u64; 25];
     let start = CycleCounter::start_measurement();
     unsafe {
-        core::hint::black_box(libcrux_pqm4::KeccakF1600_StatePermute(
+        core::hint::black_box(libcrux_pqm4::KeccakF1600_StatePermute_4Rounds(
             core::hint::black_box(sha3_state.as_mut_ptr()),
         ));
     }
-    CycleCounter::end_measurement("pqm4: KeccakF1600_StatePermute", start);
+    CycleCounter::end_measurement("pqm4: KeccakF1600_StatePermute_4Rounds", start);
+
+    let mut sha3_state = libcrux_sha3::portable::incremental::shake128_init();
+    let start = CycleCounter::start_measurement();
+    unsafe {
+        core::hint::black_box(libcrux_sha3::portable::incremental::keccakf1660_4rounds(
+            core::hint::black_box(&mut sha3_state),
+        ));
+    }
+    CycleCounter::end_measurement("libcrux: keccakf1600_4rounds", start);
 
     assert_eq!(ss_enc, ss_dec);
 
