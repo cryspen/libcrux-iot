f7d3ca8bcdfaba3cffe07f7f39cd8f883bc6fb82
Jan Winkelmann (keks) <jan@cryspen.com>
2025-04-08 11:17:46 +0200

    make model output more informative

--- m4-cycle-model/src/main.rs
+++ m4-cycle-model/src/main.rs
@@ -29,32 +29,42 @@ impl<'a> Instruction<'a> {
     }
 }
 
-fn count_cycles<'a>(instrs: impl Iterator<Item = Instruction<'a>>) -> usize {
+struct CycleReport {
+    pub normal: usize,
+    pub ppln_ld: usize,
+    pub ppln_str: usize,
+}
+
+fn count_cycles<'a>(instrs: impl Iterator<Item = Instruction<'a>>) -> CycleReport {
     let mut in_load_run = false;
-    let mut cycles = 0;
+    let mut cycles = CycleReport {
+        normal: 0,
+        ppln_ld: 0,
+        ppln_str: 0,
+    };
 
     for instr in instrs {
         match instr {
             Instruction::Ld => {
                 // per-run penalty
                 if !in_load_run {
-                    cycles += 1;
+                    cycles.ppln_ld += 1;
                     in_load_run = true;
                 }
 
-                cycles += 1
+                cycles.normal += 1
             }
             Instruction::Str => {
                 // store can be pipelined at the end of a load run
                 if !in_load_run {
-                    cycles += 1;
+                    cycles.ppln_str += 1;
                 }
 
                 in_load_run = false;
-                cycles += 1;
+                cycles.normal += 1;
             }
             _ => {
-                cycles += 1;
+                cycles.normal += 1;
                 in_load_run = false;
             }
         }
@@ -65,7 +75,16 @@ fn count_cycles<'a>(instrs: impl Iterator<Item = Instruction<'a>>) -> usize {
 
 fn main() {
     let lines: Vec<String> = std::io::stdin().lines().map(|res| res.unwrap()).collect();
+    let CycleReport {
+        normal,
+        ppln_ld,
+        ppln_str,
+    } = count_cycles(lines.iter().map(String::as_str).map(Instruction::from_str));
+    let total = normal + ppln_str + ppln_ld;
 
-    let cycles = count_cycles(lines.iter().map(String::as_str).map(Instruction::from_str));
-    println!("{cycles}");
+    println!("normal    pipeline (ld)     pipeline (str)      total");
+    println!(
+        "{: >6}    {: >13}     {: >14}    {: >7}",
+        normal, ppln_ld, ppln_str, total,
+    );
 }
