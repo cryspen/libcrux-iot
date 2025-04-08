5e47b8ed9de7f95435598de0abfafa2a2ca41705
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-04-08 10:15:39 +0200

    Debug state

--- libcrux/libcrux-sha3/src/alloc.rs
+++ libcrux/libcrux-sha3/src/alloc.rs
@@ -28,6 +28,8 @@ impl<const STACK_SIZE: usize, T: Sized + Default + Copy> Alloc<STACK_SIZE, T> {
 
     pub(crate) fn alloc(&self, len: usize) -> &mut [T] {
         println!("Allocation {len}");
+        println!("self.buffer: {:?}", self.buffer.as_ptr());
+        println!("self.pointer: {:?}", *self.pointer.borrow());
         let allocation_start = *self.pointer.borrow();
         let allocation_end = unsafe { (*self.pointer.borrow()).add(len ) };
 
@@ -38,17 +40,21 @@ impl<const STACK_SIZE: usize, T: Sized + Default + Copy> Alloc<STACK_SIZE, T> {
         let out: &mut [T] =
             unsafe { core::slice::from_raw_parts_mut(allocation_start, len) };
 
-        *self.pointer.borrow_mut() = unsafe { (*self.pointer.borrow()).add(len ) };
+        *self.pointer.borrow_mut() = unsafe { allocation_start.add(len ) };
         
         out
     }
 
     /// This assumes that `allocation` was the most recent allocation.
     pub(crate) fn free(&self, allocation: &mut [T]) {
+        println!("Freeing {}", allocation.len());
+        println!("self.buffer: {:?}", self.buffer.as_ptr());
+        println!("self.pointer: {:?}", *self.pointer.borrow());
+        
         let new_pointer =
             unsafe { (*self.pointer.borrow()).offset(-(allocation.len() as isize)) };
-        if (new_pointer as *const T) < self.buffer.as_ptr() {
-            panic!("Trying to free more than was allocated")
+        if (new_pointer as *const T) != allocation.as_ptr() {
+            panic!("Invalid free")
         }
 
         *self.pointer.borrow_mut() = new_pointer;
