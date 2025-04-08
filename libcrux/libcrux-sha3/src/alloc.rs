93e87ff5efe095bd0c83ceeaa86b34d188505986
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-04-08 12:04:27 +0200

    Late initialization of buffer pointer

--- libcrux/libcrux-sha3/src/alloc.rs
+++ libcrux/libcrux-sha3/src/alloc.rs
@@ -3,45 +3,40 @@
 use core::cell::RefCell;
 use core::default::Default;
 
-pub struct Alloc<const STACK_SIZE: usize, T:Sized + Default + Copy> {
+pub struct Alloc<const STACK_SIZE: usize, T: Sized + Default + Copy> {
     /// The backing buffer.
     #[allow(dead_code)]
     buffer: [T; STACK_SIZE],
     /// Points to the region of available memory within [buffer].
     pointer: RefCell<*mut T>,
-    /// Points to the end of [buffer].
-    end_pointer: *const T,
 }
 
 impl<const STACK_SIZE: usize, T: Sized + Default + Copy> Alloc<STACK_SIZE, T> {
     pub(crate) fn new() -> Self {
-        let mut buffer = [T::default(); STACK_SIZE];
-        let pointer = RefCell::new(buffer.as_mut_ptr());
-        let end_pointer = unsafe { buffer.as_mut_ptr().add(STACK_SIZE) };
+        let buffer = [T::default(); STACK_SIZE];
+        let pointer = RefCell::new(core::ptr::null_mut::<T>());
 
-        Self {
-            buffer,
-            pointer,
-            end_pointer,
-        }
+        let out = Self { buffer, pointer };
+
+        let buffer_ptr = out.buffer.as_ptr();
+
+        *(out.pointer.borrow_mut()) = buffer_ptr as *mut T;
+
+        out
     }
 
     pub(crate) fn alloc(&self, len: usize) -> &mut [T] {
-        println!("Allocation {len}");
-        println!("self.buffer: {:?}", self.buffer.as_ptr());
-        println!("self.pointer: {:?}", *self.pointer.borrow());
         let allocation_start = *self.pointer.borrow();
-        let allocation_end = unsafe { (*self.pointer.borrow()).add(len ) };
+        let allocation_end = unsafe { (*self.pointer.borrow()).add(len) };
 
-        if (allocation_end as *const T) >= self.end_pointer {
+        if (allocation_end as *const T) >= unsafe { self.buffer.as_ptr().add(STACK_SIZE) } {
             panic!("Insufficient memory")
         }
 
-        let out: &mut [T] =
-            unsafe { core::slice::from_raw_parts_mut(allocation_start, len) };
+        let out: &mut [T] = unsafe { core::slice::from_raw_parts_mut(allocation_start, len) };
 
-        *self.pointer.borrow_mut() = unsafe { allocation_start.add(len ) };
-        
+        *self.pointer.borrow_mut() = allocation_end;
+
         out
     }
 
@@ -50,9 +45,8 @@ impl<const STACK_SIZE: usize, T: Sized + Default + Copy> Alloc<STACK_SIZE, T> {
         println!("Freeing {}", allocation.len());
         println!("self.buffer: {:?}", self.buffer.as_ptr());
         println!("self.pointer: {:?}", *self.pointer.borrow());
-        
-        let new_pointer =
-            unsafe { (*self.pointer.borrow()).offset(-(allocation.len() as isize)) };
+
+        let new_pointer = unsafe { (*self.pointer.borrow()).sub(allocation.len()) };
         if (new_pointer as *const T) != allocation.as_ptr() {
             panic!("Invalid free")
         }
