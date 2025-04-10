be0664ddcc855c3b130de47418e3f47ea029fd70
Jan Winkelmann (keks) <jan@cryspen.com>
2025-04-10 13:38:06 +0200

    remove old code

--- libcrux/libcrux-sha3/src/generic_keccak.rs
+++ libcrux/libcrux-sha3/src/generic_keccak.rs
@@ -232,143 +232,7 @@ impl<const PARALLEL_LANES: usize, const RATE: usize, STATE: KeccakStateItem<PARA
     }
 }
 
-/// From here, everything is generic
-///
-const _ROTC: [usize; 24] = [
-    1, 62, 28, 27, 36, 44, 6, 55, 20, 3, 10, 43, 25, 39, 41, 45, 15, 21, 8, 18, 2, 61, 56, 14,
-];
-
-#[inline(always)]
-pub(crate) fn theta_rho<const N: usize, T: KeccakStateItem<N>>(s: &mut KeccakState<N, T>) {
-    let c: [T; 5] = [
-        T::xor5(
-            s.get(0, 0),
-            s.get(1, 0),
-            s.get(2, 0),
-            s.get(3, 0),
-            s.get(4, 0),
-        ),
-        T::xor5(
-            s.get(0, 1),
-            s.get(1, 1),
-            s.get(2, 1),
-            s.get(3, 1),
-            s.get(4, 1),
-        ),
-        T::xor5(
-            s.get(0, 2),
-            s.get(1, 2),
-            s.get(2, 2),
-            s.get(3, 2),
-            s.get(4, 2),
-        ),
-        T::xor5(
-            s.get(0, 3),
-            s.get(1, 3),
-            s.get(2, 3),
-            s.get(3, 3),
-            s.get(4, 3),
-        ),
-        T::xor5(
-            s.get(0, 4),
-            s.get(1, 4),
-            s.get(2, 4),
-            s.get(3, 4),
-            s.get(4, 4),
-        ),
-    ];
-    #[allow(clippy::identity_op)]
-    let t: [T; 5] = [
-        T::rotate_left1_and_xor(c[(0 + 4) % 5], c[(0 + 1) % 5]),
-        T::rotate_left1_and_xor(c[(1 + 4) % 5], c[(1 + 1) % 5]),
-        T::rotate_left1_and_xor(c[(2 + 4) % 5], c[(2 + 1) % 5]),
-        T::rotate_left1_and_xor(c[(3 + 4) % 5], c[(3 + 1) % 5]),
-        T::rotate_left1_and_xor(c[(4 + 4) % 5], c[(4 + 1) % 5]),
-    ];
-
-    s.set(0, 0, T::xor(s.get(0, 0), t[0]));
-    s.set(1, 0, T::xor_and_rotate::<36, 28>(s.get(1, 0), t[0]));
-    s.set(2, 0, T::xor_and_rotate::<3, 61>(s.get(2, 0), t[0]));
-    s.set(3, 0, T::xor_and_rotate::<41, 23>(s.get(3, 0), t[0]));
-    s.set(4, 0, T::xor_and_rotate::<18, 46>(s.get(4, 0), t[0]));
-
-    s.set(0, 1, T::xor_and_rotate::<1, 63>(s.get(0, 1), t[1]));
-    s.set(1, 1, T::xor_and_rotate::<44, 20>(s.get(1, 1), t[1]));
-    s.set(2, 1, T::xor_and_rotate::<10, 54>(s.get(2, 1), t[1]));
-    s.set(3, 1, T::xor_and_rotate::<45, 19>(s.get(3, 1), t[1]));
-    s.set(4, 1, T::xor_and_rotate::<2, 62>(s.get(4, 1), t[1]));
-
-    s.set(0, 2, T::xor_and_rotate::<62, 2>(s.get(0, 2), t[2]));
-    s.set(1, 2, T::xor_and_rotate::<6, 58>(s.get(1, 2), t[2]));
-    s.set(2, 2, T::xor_and_rotate::<43, 21>(s.get(2, 2), t[2]));
-    s.set(3, 2, T::xor_and_rotate::<15, 49>(s.get(3, 2), t[2]));
-    s.set(4, 2, T::xor_and_rotate::<61, 3>(s.get(4, 2), t[2]));
-
-    s.set(0, 3, T::xor_and_rotate::<28, 36>(s.get(0, 3), t[3]));
-    s.set(1, 3, T::xor_and_rotate::<55, 9>(s.get(1, 3), t[3]));
-    s.set(2, 3, T::xor_and_rotate::<25, 39>(s.get(2, 3), t[3]));
-    s.set(3, 3, T::xor_and_rotate::<21, 43>(s.get(3, 3), t[3]));
-    s.set(4, 3, T::xor_and_rotate::<56, 8>(s.get(4, 3), t[3]));
-
-    s.set(0, 4, T::xor_and_rotate::<27, 37>(s.get(0, 4), t[4]));
-    s.set(1, 4, T::xor_and_rotate::<20, 44>(s.get(1, 4), t[4]));
-    s.set(2, 4, T::xor_and_rotate::<39, 25>(s.get(2, 4), t[4]));
-    s.set(3, 4, T::xor_and_rotate::<8, 56>(s.get(3, 4), t[4]));
-    s.set(4, 4, T::xor_and_rotate::<14, 50>(s.get(4, 4), t[4]));
-}
-
-const _PI: [usize; 24] = [
-    6, 12, 18, 24, 3, 9, 10, 16, 22, 1, 7, 13, 19, 20, 4, 5, 11, 17, 23, 2, 8, 14, 15, 21,
-];
-
-#[inline(always)]
-pub(crate) fn pi<const N: usize, T: KeccakStateItem<N>>(s: &mut KeccakState<N, T>) {
-    let old = s.clone();
-    s.set(1, 0, old.get(0, 3));
-    s.set(2, 0, old.get(0, 1));
-    s.set(3, 0, old.get(0, 4));
-    s.set(4, 0, old.get(0, 2));
-    s.set(0, 1, old.get(1, 1));
-    s.set(1, 1, old.get(1, 4));
-    s.set(2, 1, old.get(1, 2));
-    s.set(3, 1, old.get(1, 0));
-    s.set(4, 1, old.get(1, 3));
-    s.set(0, 2, old.get(2, 2));
-    s.set(1, 2, old.get(2, 0));
-    s.set(2, 2, old.get(2, 3));
-    s.set(3, 2, old.get(2, 1));
-    s.set(4, 2, old.get(2, 4));
-    s.set(0, 3, old.get(3, 3));
-    s.set(1, 3, old.get(3, 1));
-    s.set(2, 3, old.get(3, 4));
-    s.set(3, 3, old.get(3, 2));
-    s.set(4, 3, old.get(3, 0));
-    s.set(0, 4, old.get(4, 4));
-    s.set(1, 4, old.get(4, 2));
-    s.set(2, 4, old.get(4, 0));
-    s.set(3, 4, old.get(4, 3));
-    s.set(4, 4, old.get(4, 1));
-}
-
-#[inline(always)]
-pub(crate) fn chi<const N: usize, T: KeccakStateItem<N>>(s: &mut KeccakState<N, T>) {
-    let old = s.clone();
-
-    #[allow(clippy::needless_range_loop)]
-    for i in 0..5 {
-        for j in 0..5 {
-            s.set(
-                i,
-                j,
-                T::and_not_xor(
-                    s.get(i, j),
-                    old.get(i, (j + 2) % 5),
-                    old.get(i, (j + 1) % 5),
-                ),
-            );
-        }
-    }
-}
+//// From here, everything is generic
 
 const ROUNDCONSTANTS: [u64; 24] = [
     0x0000_0000_0000_0001u64,
@@ -397,11 +261,7 @@ const ROUNDCONSTANTS: [u64; 24] = [
     0x8000_0000_8000_8008u64,
 ];
 
-#[inline(always)]
-pub(crate) fn iota<const N: usize, T: KeccakStateItem<N>>(s: &mut KeccakState<N, T>, i: usize) {
-    s.set(0, 0, T::xor_constant(s.get(0, 0), ROUNDCONSTANTS[i]));
-}
-
+/// N^i(x, y) from Alg. 4
 macro_rules! ni_y {
     ($i:expr, $x:literal, $y: literal) => {
         match $i {
@@ -409,17 +269,13 @@ macro_rules! ni_y {
             1 => ($x + 2 * $y) % 5,
             2 => (3 * $x + 4 * $y) % 5,
             3 => (2 * $x + 3 * $y) % 5,
+            // using compile error failed all the fime, no idea why
             _ => unreachable!("round number too high: {} > 4", $i),
         }
     };
 }
 
-macro_rules! n_y {
-    ($x:expr, $y:expr) => {
-        ($x + 2 * $y) % 5
-    };
-}
-
+/// This chooses the r[N(x, y)^T] from Alg.4
 macro_rules! xor_and_rotate {
     (0, 0, $t:ty) => {
         <$t>::xor_and_rotate::<0, 64>
@@ -498,6 +354,7 @@ macro_rules! xor_and_rotate {
     };
 }
 
+/// r[(x, y)]
 macro_rules! r {
     (3, 2, $t:ty) => {
         <$t>::xor_and_rotate::<25, 39>
