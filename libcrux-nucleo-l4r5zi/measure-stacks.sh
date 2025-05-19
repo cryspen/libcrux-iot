d8f53d8e725f0a5555235be78513412ecc30fdb0
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-05-19 17:51:11 +0200

    Some documentation for the measurement script

--- libcrux-nucleo-l4r5zi/measure-stacks.sh
+++ libcrux-nucleo-l4r5zi/measure-stacks.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This script will compile and run the stack measurement binaries in
+# release mode, writing the results to `stack.dat` (will be
+# overwritten if it exists).
+# 
+# The stack measurement binaries run the operations in sequence at the
+# moment, so if an earlier operation uses more stack than a later one,
+# the latter's measurement will be shadowed by the former's. (Or if
+# the stack is not fully cleared between operations for some reason.)
+
 echo "Starting measurements"
 echo "[ML-KEM] KeyGen"
 cargo rrb mlkem_stack_keygen --quiet --features stack > stack.dat
@@ -12,6 +21,6 @@ echo "[ML-DSA] KeyGen"
 cargo rrb mldsa_stack_keygen --quiet --features stack >> stack.dat
 echo "[ML-DSA] Sign"
 cargo rrb mldsa_stack_sign --quiet --features stack >> stack.dat
-echo "[ML-DSA] Verify Decaps"
+echo "[ML-DSA] Verify"
 cargo rrb mldsa_stack_verify --quiet --features stack >> stack.dat
 echo "Done"
