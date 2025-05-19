c5669959b9c27d0c365e8520c7f3088c00c74557
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-05-19 18:08:54 +0200

    Add `required_features` for stack binaries

--- libcrux-nucleo-l4r5zi/src/stack.rs
+++ libcrux-nucleo-l4r5zi/src/stack.rs
@@ -3,6 +3,18 @@
 const STACK_PAINT_VALUE: u32 = 0xcccc_cccc;
 
 #[inline(never)]
+/// Measure and print maximal stack usage.
+///
+/// This assumes a downward growing stack starting at `stack_start`
+/// and searches for the lowest occurence in memory of the
+/// STACK_PAINT_VALUE, which originates from `cortex-m-rt`'s
+/// `paint-stack` feature. It then prints the address right before
+/// where the marker was found and the distance in bytes from
+/// `stack_start`.
+///
+/// **SAFETY**: This function relies on inherently unsafe operations:
+///  - Reading the stack pointer for the starting point of the search via inline assembly
+///  - `read_volatile` to reliably scan memory for the paint marker
 pub fn measure(name: &str, stack_start: *const u32) {
     defmt::println!("Measurement: {}", name);
     defmt::println!("  Stack begins at: {:?}", stack_start);
