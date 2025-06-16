d2bad6d4d16a70b41f46c37f7b4ba2e5f1858427
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-06-16 08:14:38 +0200

    Cycle measurement script

---
+++ libcrux-nucleo-l4r5zi/measure-cycles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script will compile and run the cycles measurement binaries in
+# release mode, writing the results to `cycles-{$REV}.dat` (will be
+# overwritten if it exists).
+
+export REV=$(git rev-parse HEAD)
+export FILE="cycles-${REV}.dat"
+
+echo -n "Repository at commit: " > "$FILE"
+echo "$REV" >> "$FILE"
+
+echo "Starting measurements"
+echo "[ML-KEM]"
+DEFMT_LOG=info cargo rrb mlkem --quiet >> "$FILE"
+echo "Done"
