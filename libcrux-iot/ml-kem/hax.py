c828b022598ecaabf445b6cc1928abc93949d23b
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-08-14 16:35:20 +0200

    WIP F* extraction

---
+++ libcrux-iot/ml-kem/hax.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python3
+
+import os
+import argparse
+import subprocess
+import sys
+
+
+def shell(command, expect=0, cwd=None, env={}):
+    subprocess_stdout = subprocess.DEVNULL
+
+    print("Env:", env)
+    print("Command: ", end="")
+    for i, word in enumerate(command):
+        if i == 4:
+            print("'{}' ".format(word), end="")
+        else:
+            print("{} ".format(word), end="")
+
+    print("\nDirectory: {}".format(cwd))
+
+    os_env = os.environ
+    os_env.update(env)
+
+    ret = subprocess.run(command, cwd=cwd, env=os_env)
+    if ret.returncode != expect:
+        raise Exception("Error {}. Expected {}.".format(ret, expect))
+
+
+class extractAction(argparse.Action):
+
+    def __call__(self, parser, args, values, option_string=None) -> None:
+        # XXX: How to extract this, when it is an external dependency?
+        # Extract libcrux-secrets
+        # include_str = "+**"
+        # interface_include = ""
+        # cargo_hax_into = [
+        #     "cargo",
+        #     "hax",
+        #     "into",
+        #     "-i",
+        #     include_str,
+        #     "fstar",
+        # ]
+        # hax_env = {}
+        # shell(
+        #     cargo_hax_into,
+        #     cwd="../secrets",
+        #     env=hax_env,
+        # )
+
+        # Extract ml-kem
+        includes = [
+            "+**",
+            "-libcrux_ml_kem::types::index_impls::**",
+            "-libcrux_ml_kem::kem::**",
+            "-libcrux_ml_kem::hash_functions::portable::*",
+            "+:libcrux_ml_kem::hash_functions::*::*",
+        ]
+        include_str = " ".join(includes)
+        interface_include = "+** -libcrux_ml_kem::vector::traits -libcrux_ml_kem::types -libcrux_ml_kem::constants"
+        cargo_hax_into = [
+            "cargo",
+            "hax",
+            "into",
+            "-i",
+            include_str,
+            "fstar",
+            "--z3rlimit",
+            "80",
+            "--interfaces",
+            interface_include,
+        ]
+        hax_env = {}
+        shell(
+            cargo_hax_into,
+            cwd=".",
+            env=hax_env,
+        )
+        return None
+
+
+class proveAction(argparse.Action):
+
+    def __call__(self, parser, args, values, option_string=None) -> None:
+        admit_env = {}
+        if args.admit:
+            admit_env = {"OTHERFLAGS": "--admit_smt_queries true"}
+        shell(["make", "-j4", "-C", "proofs/fstar/extraction/"], env=admit_env)
+        return None
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Libcrux prove script. "
+        + "Make sure to separate sub-command arguments with --."
+    )
+    subparsers = parser.add_subparsers()
+
+    extract_parser = subparsers.add_parser(
+        "extract", help="Extract the F* code for the proofs."
+    )
+    extract_parser.add_argument("extract", nargs="*", action=extractAction)
+
+    prover_parser = subparsers.add_parser(
+        "prove",
+        help="""
+        Run F*.
+
+        This typechecks the extracted code.
+        To lax-typecheck use --admit.
+        """,
+    )
+    prover_parser.add_argument(
+        "--admit",
+        help="Admit all smt queries to lax typecheck.",
+        action="store_true",
+    )
+    prover_parser.add_argument(
+        "prove",
+        nargs="*",
+        action=proveAction,
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    return parser.parse_args()
+
+
+def main():
+    # Don't print unnecessary Python stack traces.
+    sys.tracebacklimit = 0
+    parse_arguments()
+
+
+if __name__ == "__main__":
+    main()
