95b37f8c6348dd3cf2741ced7e0dabe6fa98bc6c
Jan Winkelmann (keks) <jan@cryspen.com>
2025-10-16 17:35:56 +0200

    run script from workspace

--- c/extract-sha3.sh
+++ c/extract-sha3.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./c.sh --extract sha3 --config sha3.yml --libcrux-dep secrets --clean cfiles
+repo_root=$(git rev-parse --show-toplevel)
+
+$repo_root/c/c.sh --extract sha3 --config $repo_root/c/sha3.yml --libcrux-dep secrets --clean cfiles
