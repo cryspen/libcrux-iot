44e01c092fbef1c33b3cf4191ac0cef4fd269703
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-10-23 16:50:59 +0200

    Remainder of C extraction

---
+++ c/sha3/extracted/libcrux_iot_core.c
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Cryspen Sarl <info@cryspen.com>
+ *
+ * SPDX-License-Identifier: MIT or Apache-2.0
+ *
+ * This code was generated with the following revisions:
+ * Charon:
+ * Eurydice:
+ * Karamel:
+ * F*: unset
+ * Libcrux: 9b433af082b22712567a1ff8812d83a0e110625b
+ */
+
+#include "internal/libcrux_iot_core.h"
+
+/**
+This function found in impl {libcrux_secrets::traits::Classify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.classify_27
+with types uint64_t
+
+*/
+static KRML_MUSTINLINE uint64_t classify_27_49(uint64_t self) { return self; }
+
+/**
+This function found in impl {libcrux_secrets::traits::Declassify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.declassify_d8
+with types uint32_t
+
+*/
+static KRML_MUSTINLINE uint32_t declassify_d8_df(uint32_t self) { return self; }
+
+/**
+This function found in impl {libcrux_secrets::int::CastOps for u32}
+*/
+uint64_t libcrux_secrets_int_as_u64_b8(uint32_t self) {
+  return classify_27_49((uint64_t)declassify_d8_df(self));
+}
+
+/**
+This function found in impl {libcrux_secrets::traits::Classify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.classify_27
+with types uint32_t
+
+*/
+uint32_t libcrux_secrets_int_public_integers_classify_27_df(uint32_t self) {
+  return self;
+}
+
+/**
+This function found in impl {libcrux_secrets::traits::Declassify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.declassify_d8
+with types uint64_t
+
+*/
+static KRML_MUSTINLINE uint64_t declassify_d8_49(uint64_t self) { return self; }
+
+/**
+This function found in impl {libcrux_secrets::int::CastOps for u64}
+*/
+uint32_t libcrux_secrets_int_as_u32_a3(uint64_t self) {
+  return libcrux_secrets_int_public_integers_classify_27_df(
+      (uint32_t)declassify_d8_49(self));
+}
+
+/**
+This function found in impl {libcrux_secrets::traits::Classify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.classify_27
+with types uint8_t
+
+*/
+uint8_t libcrux_secrets_int_public_integers_classify_27_90(uint8_t self) {
+  return self;
+}
+
+/**
+This function found in impl {libcrux_secrets::traits::Declassify<T> for T}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.declassify_d8
+with types uint8_t
+
+*/
+uint8_t libcrux_secrets_int_public_integers_declassify_d8_90(uint8_t self) {
+  return self;
+}
+
+/**
+This function found in impl {libcrux_secrets::traits::ClassifyRef<&'a (T)> for
+&'a (T)}
+*/
+/**
+A monomorphic instance of libcrux_secrets.int.public_integers.classify_ref_c5
+with types Eurydice_slice uint8_t
+
+*/
+Eurydice_slice *libcrux_secrets_int_public_integers_classify_ref_c5_ba(
+    Eurydice_slice *self) {
+  return self;
+}
---
+++ c/sha3/extracted/libcrux_iot_core.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Cryspen Sarl <info@cryspen.com>
+ *
+ * SPDX-License-Identifier: MIT or Apache-2.0
+ *
+ * This code was generated with the following revisions:
+ * Charon:
+ * Eurydice:
+ * Karamel:
+ * F*: unset
+ * Libcrux: 9b433af082b22712567a1ff8812d83a0e110625b
+ */
+
+#ifndef libcrux_iot_core_H
+#define libcrux_iot_core_H
+
+#include "eurydice_glue.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct Eurydice_slice_uint8_t_x2_s {
+  Eurydice_slice fst;
+  Eurydice_slice snd;
+} Eurydice_slice_uint8_t_x2;
+
+#if defined(__cplusplus)
+}
+#endif
+
+#define libcrux_iot_core_H_DEFINED
+#endif /* libcrux_iot_core_H */
