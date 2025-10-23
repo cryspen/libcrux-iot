127c147f56ab5c4d1979b6be7578f686419dcf3d
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-10-23 17:05:35 +0200

    Remove unused trait

--- libcrux-iot/sha3/src/lib.rs
+++ libcrux-iot/sha3/src/lib.rs
@@ -382,11 +382,6 @@ pub mod portable {
 }
 
 #[cfg(feature = "check-secret-independence")]
-trait FromLeBytes<const N: usize>: Sized {
-    fn from_le_bytes(bytes: [U8; N]) -> Self;
-}
-
-#[cfg(feature = "check-secret-independence")]
 trait ToLeBytes<const N: usize>: Sized {
     fn to_le_bytes(self) -> [U8; N];
 }
