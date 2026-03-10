b6fba20f6a58691a1610ecfc079ec6bcd196b279
Alexander Bentkamp <alexander@cryspen.com>
2026-03-10 14:21:15 +0100

    sha3: add fold range

--- libcrux-iot/sha3/proofs/lean/extraction/helpers.lean
+++ libcrux-iot/sha3/proofs/lean/extraction/helpers.lean
@@ -23,3 +23,25 @@ instance :  GetElemResult (RustSlice u8) (core_models.ops.range.RangeFrom usize)
 instance : GetElemResult (RustSlice u8) (core_models.ops.range.RangeTo usize) (RustSlice u8) := sorry
 
 def core_models.num.Impl_8.MAX : u32 := sorry
+
+open Std.Do
+
+def Int32.fold_range {α : Type}
+    (s e : Int32)
+    (inv : α -> Int32 -> RustM Prop)
+    (init: α)
+    (body : α -> Int32 -> RustM α)
+    (pureInv: {i : α -> Int32 -> Prop // ∀ a b, ⦃⌜ True ⌝⦄ inv a b ⦃⇓ r => ⌜ r = (i a b) ⌝⦄})
+    : RustM α := do
+    if s < e
+    then fold_range (s + 1) e inv (← body init s) body pureInv
+    else pure init
+termination_by (e.toInt - s.toInt).toNat
+decreasing_by
+  have : (s + 1).toInt = s.toInt + 1 := by grind
+  grind
+
+@[spec]
+instance : @rust_primitives.hax.folds Int32 where
+  fold_range := Int32.fold_range
+  fold_range_return := sorry
