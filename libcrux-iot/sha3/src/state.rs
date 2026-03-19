6199355efb1a245535b38a3dbe6bd4cd072ab72a
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-03-19 16:46:56 +0100

    Revert "sha3: workaround for array-to-slice conversion"
    This reverts commit ff1ae6165863abb04d2aa4f9d04825bb3ff4510c.

--- libcrux-iot/sha3/src/state.rs
+++ libcrux-iot/sha3/src/state.rs
@@ -150,8 +150,7 @@ fn store_block_2u32<const RATE: usize>(s: &KeccakState, out: &mut [U8]) {
 
 #[inline(always)]
 fn store_block_full_2u32<const RATE: usize>(s: &KeccakState, out: &mut [U8; 200]) {
-    // `out[..]` is a workaround for https://github.com/cryspen/hax/issues/1983
-    store_block_2u32::<RATE>(s, &mut out[..]);
+    store_block_2u32::<RATE>(s, out);
 }
 
 #[cfg(feature = "check-secret-independence")]
