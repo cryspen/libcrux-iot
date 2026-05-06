84e5ad4bc4f68e57f1de2c71fe6d94ca441dd165
Alexander Bentkamp <alexander@cryspen.com>
2026-05-06 15:57:25 +0200

    mldsa lean setup

--- libcrux-iot/ml-dsa/hax_aeneas.sh
+++ libcrux-iot/ml-dsa/hax_aeneas.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# hax version: c11d79045605edbceb02377c1053e03f7c36b8c4
+# hax version: 7b4bd97058e0fcbf9135b76297ca91942f2327a6
 # aeneas version: 863909af
 
-cargo hax into aeneas-lean
+cargo hax into aeneas-lean \
+  --charon-args="\
+    --start-from crate::simd::portable::invntt::invert_ntt_montgomery\
+    --start-from crate::simd::portable::ntt::ntt\
+    --start-from crate::simd::portable::arithmetic::montgomery_multiply"
 
 sed -i \
-    's/import Aeneas/import Aeneas\nimport LibcruxIotSha3.Extraction.Missing\nopen core_models/' \
-    proofs/aeneas-lean/LibcruxIotSha3/Extraction/Funs.lean
+    's/import Aeneas/import Aeneas\nimport CoreModels\nopen core_models/' \
+    proofs/aeneas-lean/LibcruxIotMlDsa/Extraction/Funs.lean
