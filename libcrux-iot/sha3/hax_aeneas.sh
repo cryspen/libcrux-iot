0d75c1820dac279162662e353f178b68a506f7c0
Alexander Bentkamp <alexander@cryspen.com>
2026-05-07 11:13:58 +0200

    [sha3] exclude Debug formatter for Lane2U32

--- libcrux-iot/sha3/hax_aeneas.sh
+++ libcrux-iot/sha3/hax_aeneas.sh
@@ -3,7 +3,7 @@
 # hax version: 7b4bd97058e0fcbf9135b76297ca91942f2327a6
 # aeneas version: 863909af
 
-cargo hax into aeneas-lean
+RUSTFLAGS="--cfg charon" cargo hax into aeneas-lean
 
 sed -i \
     's/import Aeneas/import Aeneas\nimport LibcruxIotSha3.Extraction.Missing\nopen core_models/' \
