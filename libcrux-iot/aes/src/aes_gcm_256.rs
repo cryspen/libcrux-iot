adee71a7c2951fec82c4475b3ed3c8ef54bd9bf6
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-05-28 08:38:30 +0200

    [aes] Initial state for libcrux-iot-aes

---
+++ libcrux-iot/aes/src/aes_gcm_256.rs
@@ -0,0 +1,59 @@
+use crate::{
+    aes::AES_BLOCK_LEN,
+    aes_gcm::aesgcm,
+    ctr::Aes256CtrContext,
+    gf128::GF128State,
+    platform::{AESState, GF128FieldElement},
+    DecryptError, NONCE_LEN, TAG_LEN,
+};
+
+/// AES-GCM 256 key length.
+pub const KEY_LEN: usize = 32;
+pub(crate) const GCM_KEY_LEN: usize = 16;
+
+/// The AES-GCM 256 state
+pub(crate) struct State<T: AESState, U: GF128FieldElement> {
+    pub(crate) aes_state: Aes256CtrContext<T>,
+    pub(crate) gcm_state: GF128State<U>,
+    pub(crate) tag_mix: [u8; TAG_LEN],
+}
+
+aesgcm!(State<T, U>, Aes256CtrContext);
+
+use super::aes_gcm::type_aliases;
+
+type_aliases!(AesGcm256, "AES-GCM 256");
+
+/// # Portable implementation of AES-GCM 256
+///
+/// To use the portable implementation, `Key`, `Nonce`, and `Tag` types
+/// must be explicitely parameterized by the portable implementation.
+///
+/// Example:
+/// ```rust
+/// // Using the portable implementation.
+/// use libcrux_iot_aes::{
+///     aes_gcm_256::portable::{Key, Nonce, PortableAesGcm256, Tag},
+///     AeadConsts as _, NONCE_LEN, TAG_LEN,
+/// };
+///
+/// let k: Key<PortableAesGcm256> = [0; PortableAesGcm256::KEY_LEN].into();
+/// let nonce: Nonce<PortableAesGcm256> = [0; NONCE_LEN].into();
+/// let mut tag: Tag<PortableAesGcm256> = [0; TAG_LEN].into();
+///
+/// let pt = b"the quick brown fox jumps over the lazy dog";
+/// let mut ct = [0; 43];
+/// let mut pt_out = [0; 43];
+///
+/// k.encrypt(&mut ct, &mut tag, &nonce, b"", pt).unwrap();
+/// k.decrypt(&mut pt_out, &nonce, b"", &ct, &tag).unwrap();
+/// assert_eq!(pt, &pt_out);
+/// ```
+pub mod portable {
+    pub use libcrux_traits::aead::{
+        typed_owned::{Key, Nonce, Tag},
+        typed_refs::{KeyMut, KeyRef, NonceRef, TagMut, TagRef},
+    };
+
+    pub use crate::{implementations::PortableAesGcm256, portable::aes_gcm_256::*};
+}
---
+++ libcrux-iot/aes/src/traits_api.rs
@@ -0,0 +1,125 @@
+use libcrux_traits::aead::{arrayref, consts, slice, typed_owned};
+
+use crate::{
+    aes_gcm_128, aes_gcm_256,
+    implementations::{AesGcm128, AesGcm256, PortableAesGcm128, PortableAesGcm256},
+    NONCE_LEN, TAG_LEN,
+};
+
+/// Macro to implement the libcrux_traits public API traits
+///
+/// For the blanket impl of `typed_refs::Aead` to take place,
+/// the `$type` must implement `Copy` and `PartialEq`.
+macro_rules! impl_traits_public_api {
+    ($type:ty, $keylen:expr, $taglen:expr, $noncelen:expr) => {
+        // prerequisite for typed_owned::Aead
+        impl consts::AeadConsts for $type {
+            const KEY_LEN: usize = $keylen;
+            const TAG_LEN: usize = $taglen;
+            const NONCE_LEN: usize = $noncelen;
+        }
+        // implement typed_owned::Aead
+        typed_owned::impl_aead_typed_owned!($type, $keylen, $taglen, $noncelen);
+    };
+}
+
+/// Macro to implement the different structs and multiplexing.
+macro_rules! api {
+    ($mod_name:ident, $variant:ident, $multiplexing:ty, $portable:ident) => {
+        mod $mod_name {
+            use super::*;
+            use libcrux_secrets::U8;
+
+            use libcrux_traits::aead::arrayref::{DecryptError, EncryptError, KeyGenError};
+            use $variant::KEY_LEN;
+
+            pub type Key = [u8; KEY_LEN];
+            pub type Tag = [u8; TAG_LEN];
+            pub type Nonce = [u8; NONCE_LEN];
+
+            mod _libcrux_traits_apis_multiplex {
+                use super::*;
+
+                // implement `libcrux_traits` slice trait
+                slice::impl_aead_slice_trait!($multiplexing => KEY_LEN, TAG_LEN, NONCE_LEN);
+
+                // implement `libcrux_traits` public API traits
+                impl_traits_public_api!($multiplexing, KEY_LEN, TAG_LEN, NONCE_LEN);
+
+                /// The plaintext length must be equal to the ciphertext length.
+                impl arrayref::Aead<KEY_LEN, TAG_LEN, NONCE_LEN> for $multiplexing {
+                    fn keygen(key: &mut [u8; KEY_LEN], rand: &[u8; KEY_LEN]) -> Result<(), KeyGenError> {
+                        *key = *rand;
+                        Ok(())
+                    }
+
+                    fn encrypt(
+                        ciphertext: &mut [u8],
+                        tag: &mut Tag,
+                        key: &Key,
+                        nonce: &Nonce,
+                        aad: &[u8],
+                        plaintext: &[u8],
+                    ) -> Result<(), EncryptError> {
+                        $portable::encrypt(ciphertext, tag, key, nonce, aad, plaintext)
+                    }
+
+                    fn decrypt(
+                        plaintext: &mut [u8],
+                        key: &Key,
+                        nonce: &Nonce,
+                        aad: &[u8],
+                        ciphertext: &[u8],
+                        tag: &Tag,
+                    ) -> Result<(), DecryptError> {
+                        $portable::decrypt(plaintext, key, nonce, aad, ciphertext, tag)
+                    }
+                }
+            }
+
+            mod _libcrux_traits_apis_portable {
+                use super::*;
+
+                // implement `libcrux_traits` slice trait
+                slice::impl_aead_slice_trait!($portable => KEY_LEN, TAG_LEN, NONCE_LEN);
+
+                // implement `libcrux_traits` public API traits
+                impl_traits_public_api!($portable, KEY_LEN, TAG_LEN, NONCE_LEN);
+
+                /// The plaintext length must be equal to the ciphertext length.
+                impl arrayref::Aead<KEY_LEN, TAG_LEN, NONCE_LEN> for $portable {
+                    fn keygen(key: &mut [u8; KEY_LEN], rand: &[u8; KEY_LEN]) -> Result<(), KeyGenError> {
+                        *key = *rand;
+                        Ok(())
+                    }
+
+                    fn encrypt(
+                        ciphertext: &mut [u8],
+                        tag: &mut Tag,
+                        key: &Key,
+                        nonce: &Nonce,
+                        aad: &[u8],
+                        plaintext: &[u8],
+                    ) -> Result<(), EncryptError> {
+                        crate::portable::$variant::encrypt(key, nonce, aad, plaintext, ciphertext, tag)
+                    }
+
+                    fn decrypt(
+                        plaintext: &mut [u8],
+                        key: &Key,
+                        nonce: &Nonce,
+                        aad: &[u8],
+                        ciphertext: &[u8],
+                        tag: &Tag,
+                    ) -> Result<(), DecryptError> {
+                        crate::portable::$variant::decrypt(key, nonce, aad, ciphertext, tag, plaintext)
+                    }
+                }
+            }
+        }
+    };
+}
+
+api!(aes128, aes_gcm_128, AesGcm128, PortableAesGcm128);
+
+api!(aes256, aes_gcm_256, AesGcm256, PortableAesGcm256);
